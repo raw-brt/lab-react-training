d75c7e3876a11cbd205cf1442199b494b6863144
Roberto Díaz <robertodzbt@gmail.com>
2020-01-05 12:22:09 +0100

    iDcard component working

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import IdCard from './components/IdCard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>IdCard</h1>
-        {/* TODO: Use the IdCard component */}
+        <IdCard 
+        image="https://pbs.twimg.com/profile_images/1155119357699248129/Hj7GwSya_400x400.jpg"
+        firstName="Roberto"
+        lastName="Diaz"
+        gender="Male"
+        height="1.81"
+        birth="07/05/1988"
+        />
 
         <h1>Greetings</h1>
         {/* TODO: Use the Greetings component */}
---
+++ starter-code/src/components/IdCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class IdCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      profilePic: this.props.image,
+      firstName: this.props.firstName,
+      lastName: this.props.lastName,
+      gender: this.props.gender,
+      height: this.props.height,
+      birth: this.props.birth
+    }
+  }
+  render() {
+    return (
+      <div className="idCard">
+        <div className="profileImg">
+          <img src={this.state.profilePic} alt=""></img>
+        </div>
+        <div className="rightColumn">
+          <p><strong>First Name: </strong>{this.state.firstName}</p>
+          <p><strong>Last Name: </strong>{this.state.lastName}</p>
+          <p><strong>Gender: </strong>{this.state.gender}</p>
+          <p><strong>Height: </strong>{this.state.height}</p>
+          <p><strong>Birth Date: </strong>{this.state.birth}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default IdCard;
